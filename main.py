8f9a99b99834f45897efd26c5060c06fa6f11491
Mohammed Mansour <66431210+MohammedMansour64@users.noreply.github.com>
2022-12-10 19:40:18 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-# write your code here
+# part 1
+
+person = [{
+    'name': 'Mohammed Mansour',
+    'age': '19',
+    'hobbies': ['Birds' , 'gaming' , 'android']
+},
+]
+
+
+print(person[0]['name'])
+print(person[0]['age'])
+
+# part 2
+
+person[0]['age'] = '20'
+
+person[0]['country'] = 'portugal'
+
+print(person)
+print(len(person))
+
+# part 3
+
+person[0]['hobbies'].append("Minecraft")
+
+
+def check_hobbies(person):
+    if len(person[0]['hobbies']) > 3:
+        print("WOW YOU ARE AMAZING")
+
+check_hobbies(person)
+
+
+
